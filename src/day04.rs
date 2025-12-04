3081d445f08a0da7ef109a77516eaedc84822c8d
jtaub <JakeTaubner@ProtonMail.com>
2025-12-04 14:45:13 -0500

    solve day 4

---
+++ src/day04.rs
@@ -0,0 +1,84 @@
+use std::{
+    fs::File,
+    io::{self, BufRead},
+};
+
+const PAPER: char = '@';
+
+pub fn solve_day_four() -> i32 {
+    solve("/home/jtaubner/RustroverProjects/advent-of-code-2025/src/input/day04.txt")
+}
+
+const DIRECTIONS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
+fn solve(filename: &str) -> i32 {
+    // Read the contents of the file into a 2D char array.
+    let grid = read_file_contents(filename);
+
+    // Solve the puzzle using the grid.
+    let mut result = 0;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] != PAPER {
+                continue;
+            }
+
+            let mut adjacent_count = 0;
+
+            for (dx, dy) in DIRECTIONS {
+                if let (Some(x), Some(y)) = (i.checked_add_signed(dx), j.checked_add_signed(dy)) {
+                    if x < grid.len() && y < grid[x].len() && grid[x][y] == PAPER {
+                        adjacent_count += 1;
+                    }
+                }
+            }
+
+            if adjacent_count < 4 {
+                result += 1;
+            }
+        }
+    }
+
+    result
+}
+
+fn read_file_contents(filename: &str) -> Vec<Vec<char>> {
+    let file = File::open(filename).expect("Failed to open file");
+    let lines = io::BufReader::new(file).lines();
+    let mut grid = Vec::new();
+    for line in lines {
+        let line = line.expect("Failed to read line");
+        let row = line.chars().collect::<Vec<char>>();
+        grid.push(row);
+    }
+
+    grid
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_day_four_example() {
+        assert_eq!(
+            solve("/home/jtaubner/RustroverProjects/advent-of-code-2025/src/input/day04test.txt"),
+            13
+        );
+    }
+
+    #[test]
+    fn test_solve_day_four() {
+        assert_eq!(solve_day_four(), 1478);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day02;
 mod day02_2;
 mod day03;
 mod day03_2;
+mod day04;
 
 fn main() {
     println!("Day 1: {}", day01::solve_day_one());
@@ -12,4 +13,5 @@ fn main() {
     println!("Day 2 part 2: {}", day02_2::solve_day_two_part_two());
     println!("Day 3: {}", day03::solve_day_three());
     println!("Day 3 part 2: {}", day03_2::solve_day_three_part_two());
+    println!("Day 4: {}", day04::solve_day_four());
 }
